8abed654a692357a5a4c6c807c59e0bede12806a
aanmeba <aanmeba@gmail.com>
2022-07-11 14:00:58 +1000

    codes basic behaviour

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -5,7 +5,19 @@ const LinkForm = () => {
     title: "",
     linkAddress: "",
   };
+
+  const [switchBtn, setSwitchBtn] = useState(false);
   const [formData, setFormData] = useState(initialFormState);
+
+  // toggle the switch
+  const handleSwitchBtn = () => {
+    setSwitchBtn(!switchBtn);
+  };
+
+  const clearForm = () => {
+    setFormData(initialFormState);
+  };
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -21,36 +33,52 @@ const LinkForm = () => {
     console.log("works!");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <>
+      <button onClick={handleSwitchBtn}>
+        {switchBtn ? "Inactive" : "Active"}
+      </button>
+      <button onClick={clearForm}>Clear</button>
+      <form onSubmit={handleSubmit} className="link-form">
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Link title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="linkAddress">Link Address: </label>
+          <input
+            type="text"
+            name="linkAddress"
+            // defaultValue="https://"
+            placeholder="Link Address"
+            value={formData.linkAddress}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit" value="Submit">
+            Submit
+          </button>
+        </div>
+      </form>
+      {/* display */}
       <div>
-        <label htmlFor="title">Title: </label>
-        <input
-          type="text"
-          name="title"
-          placeholder="Link title"
-          value={formData.title}
-          onChange={handleChange}
-          required
-        />
+        <a
+          href={formData.linkAddress}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {formData.title}
+        </a>
       </div>
-      <div>
-        <label htmlFor="linkAddress">Link Address: </label>
-        <input
-          type="text"
-          name="linkAddress"
-          // defaultValue="https://"
-          placeholder="Link Address"
-          value={formData.linkAddress}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <button type="submit" value="Submit">
-          Submit
-        </button>
-      </div>
-    </form>
+    </>
   );
 };
 
