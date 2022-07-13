eb7a86a324d2efe183d0a1ff901d8aee147ab94d
aanmeba <aanmeba@gmail.com>
2022-07-13 15:07:55 +1000

    displays created links on the preview section

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 
-const Preview = () => {
+const Preview = ({ links }) => {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <h1>Preview</h1>
-    </>
+      {links.map((link) => (
+        <a
+          key={link.id}
+          href={link.link_address}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {link.title}
+        </a>
+      ))}
+    </div>
   );
 };
 
