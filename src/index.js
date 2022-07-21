fe11adcaeb64a7ad7dd32a05ab0058d29a634284
aanmeba <aanmeba@gmail.com>
2022-07-21 17:19:47 +1000

    resolve some of warnings

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Card from "./components/Card";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Card /> */}
   </React.StrictMode>
 );
