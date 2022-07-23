cc6989fc4910f0e59423f452d3bf8f7532561aad
aanmeba <aanmeba@gmail.com>
2022-07-23 14:08:47 +1000

    edit routes

--- src/services/appearanceServices.js
+++ src/services/appearanceServices.js
@@ -2,7 +2,7 @@ import iLinkAPI from "../config/api";
 
 export const createAppearance = async (data) => {
   console.log("createAppearance -service triggered");
-  const response = await iLinkAPI.post("/dashboard/appearances", data);
+  const response = await iLinkAPI.post("/dashboard/appearance", data);
   console.log("**** createAppearance - response.data ***** ");
   console.log(response.data);
 
