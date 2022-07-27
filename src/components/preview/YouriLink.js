55e34c75a68239ca1ba6d8ebb87c67f2eab5b7dc
aanmeba <aanmeba@gmail.com>
2022-07-27 17:25:01 +1000

    retrieve data and Card component

--- src/components/preview/YouriLink.js
+++ src/components/preview/YouriLink.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getAniLink } from "../../services/linksServices";
 import Card from "./Card";
 
-const YouriLink = (props) => {
+const YouriLink = () => {
+  const { username } = useParams();
+  console.log(username);
+
+  const [loading, setLoading] = useState(true);
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    getAniLink(username)
+      .then((data) => {
+        console.log(data);
+        setApiData(data);
+        setLoading(false);
+      })
+      .catch((e) => console.log(e));
+  }, [username]);
+
   return (
     <section className="">
-      <h1>Your iLink Page</h1>
+      {loading ? (
+        <h1>Your iLink Page</h1>
+      ) : (
+        <Card links={apiData.links} appearance={apiData.appearance} />
+      )}
     </section>
   );
 };
