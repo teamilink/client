5bdbd94058feea940c6467b2695dbd7055cec5dd
aanmeba <aanmeba@gmail.com>
2022-07-28 11:24:16 +1000

    add mobile frame to the preview

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,18 +1,21 @@
 // import { Container } from "@mui/material";
 import React from "react";
+import { useGlobalState } from "../../utils/stateContext";
 // import Card from "./Card";
 import CardCopy from "./CardCopy";
 import styles from "./Preview.module.css";
 
 const Preview = ({ links, appearance }) => {
   console.log("Preview");
+  const { store } = useGlobalState();
+  const { loggedInUser } = store;
 
   return (
     <section className={styles.preview}>
       {/* <Card links={links} appearance={appearance} /> */}
       <iframe
         className={styles.frame}
-        src="http://localhost:3000/jungtotheah"
+        src={`http://localhost:3000/${loggedInUser}`}
         title="preview"
         frameBorder="0"
         allowFullScreen
