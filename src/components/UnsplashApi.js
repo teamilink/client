0d6c825c9724f78f64f9dba2d41dc8d391a86ab9
jonathan5057 <GCAB022114@coderacademy.edu.au>
2022-08-02 19:45:14 +1000

    implement an fetch api function with asynchronous

---
+++ src/components/UnsplashApi.js
@@ -0,0 +1,14 @@
+import { usestate } from 'react';
+import axios from 'axios'
+
+const UnsplashApi = () => {
+
+    const fetchAPI = async () => {
+        const response = await axios.get('');
+    }
+
+    return ();
+
+}
+
+export default UnsplashApi
